daa54bfd16f279d91f3c87f00a5c0257020a3339
Woz4tetra <woz4tetra@gmail.com>
2023-10-28 12:59:53 -0400

    image conversion

--- src/bw_object_estimation/include/bw_object_estimation.h
+++ src/bw_object_estimation/include/bw_object_estimation.h
@@ -7,6 +7,10 @@
 #include <message_filters/subscriber.h>
 #include <message_filters/time_synchronizer.h>
 
+#include <image_geometry/pinhole_camera_model.h>
+
+#include <cv_bridge/cv_bridge.h>
+
 #include <bw_interfaces/SegmentationInstanceArray.h>
 
 
@@ -15,16 +19,18 @@ class ObjectEstimation
 private:
     ros::NodeHandle nh;  // ROS node handle
 
-    typedef message_filters::sync_policies::ExactTime<sensor_msgs::Image, sensor_msgs::Image, bw_interfaces::SegmentationInstanceArray> ExactSyncPolicy;
+    typedef message_filters::sync_policies::ExactTime<sensor_msgs::Image, bw_interfaces::SegmentationInstanceArray> ExactSyncPolicy;
     typedef message_filters::Synchronizer<ExactSyncPolicy> Sync;
     boost::shared_ptr<Sync> _sync;
 
-    message_filters::Subscriber<sensor_msgs::Image> _color_sub;
+    ros::Subscriber _depth_info_sub;
     message_filters::Subscriber<sensor_msgs::Image> _depth_sub;
     message_filters::Subscriber<bw_interfaces::SegmentationInstanceArray> _segmentation_sub;
 
+    image_geometry::PinholeCameraModel _camera_model;
+
+    void camera_info_callback(const sensor_msgs::CameraInfoConstPtr& camera_info);
     void synced_callback(
-        const sensor_msgs::ImageConstPtr& color_image,
         const sensor_msgs::ImageConstPtr& depth_image,
         const bw_interfaces::SegmentationInstanceArrayConstPtr& segmentation);
 public:
--- src/bw_object_estimation/src/bw_object_estimation.cpp
+++ src/bw_object_estimation/src/bw_object_estimation.cpp
@@ -7,12 +7,13 @@ ObjectEstimation::ObjectEstimation(ros::NodeHandle* nodehandle) :
     int queue_size;
     ros::param::param<int>("~queue_size", queue_size, 10);
 
-    _color_sub.subscribe(nh, "color/image_raw", queue_size);
+    _depth_info_sub = nh.subscribe<sensor_msgs::CameraInfo>("depth/camera_info", 1, &ObjectEstimation::camera_info_callback, this);
+
     _depth_sub.subscribe(nh, "depth/image_raw", queue_size);
     _segmentation_sub.subscribe(nh, "segmentation", queue_size);
 
-    _sync.reset(new Sync(ExactSyncPolicy(queue_size), _color_sub, _depth_sub, _segmentation_sub));
-    _sync->registerCallback(boost::bind(&ObjectEstimation::synced_callback, this, _1, _2, _3));
+    _sync.reset(new Sync(ExactSyncPolicy(queue_size), _depth_sub, _segmentation_sub));
+    _sync->registerCallback(boost::bind(&ObjectEstimation::synced_callback, this, _1, _2));
 
     ROS_INFO("ObjectEstimation node initialized");
 }
@@ -21,12 +22,33 @@ ObjectEstimation::~ObjectEstimation()
 {
 }
 
+void ObjectEstimation::camera_info_callback(const sensor_msgs::CameraInfoConstPtr& camera_info)
+{
+    _camera_model.fromCameraInfo(camera_info);
+    _depth_info_sub.shutdown();
+    ROS_INFO("Camera model loaded");
+}
+
 void ObjectEstimation::synced_callback(
-    const sensor_msgs::ImageConstPtr& color_image,
     const sensor_msgs::ImageConstPtr& depth_image,
     const bw_interfaces::SegmentationInstanceArrayConstPtr& segmentation)
 {
-    ROS_INFO("Received RGBD image and segmentation");
+    if (!_camera_model.initialized())
+    {
+        ROS_WARN("Camera model not loaded yet");
+        return;
+    }
+    
+    cv_bridge::CvImagePtr depth_ptr;
+    try {
+        depth_ptr = cv_bridge::toCvCopy(depth_image);  // encoding: passthrough
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        ROS_ERROR("Failed to convert depth image: %s", e.what());
+        return;
+    }
+    ROS_INFO("Received image and segmentation");
 }
 
 
