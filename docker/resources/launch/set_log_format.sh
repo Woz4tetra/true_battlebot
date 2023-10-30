4fa6056f32a8aa7526e93f1aa72ab8027f54c3fe
Woz4tetra <woz4tetra@gmail.com>
2023-10-29 22:45:43 -0400

    fix multiline logging

--- docker/resources/launch/set_log_format.sh
+++ docker/resources/launch/set_log_format.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-export ROSCONSOLE_FORMAT='{"severity": "${severity}", "message": "${message}", "time": ${time}, "walltime": ${walltime}, "thread": "${thread}", "logger": "${logger}", "file": "${file}", "line": ${line}, "function": "${function}", "node": "${node}"}'
+export ROSCONSOLE_FORMAT='{"severity": "${severity}", "message": "<-<${message}>->", "time": ${time}, "walltime": ${walltime}, "thread": "${thread}", "logger": "${logger}", "file": "${file}", "line": ${line}, "function": "${function}", "node": "${node}"}'
+export ROSCONSOLE_CONFIG_FILE=/opt/${ORGANIZATION}/rosconsole.config
