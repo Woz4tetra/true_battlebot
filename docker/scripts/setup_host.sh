ae65d87774699b1424829bf8bf280564e1384ba6
Katelyn Salvatori <katelyn.salvatori@gmail.com>
2023-11-18 12:48:05 -0500

    Fixed small issues with setup_host.sh

--- docker/scripts/setup_host.sh
+++ docker/scripts/setup_host.sh
@@ -4,7 +4,7 @@ BASE_DIR=$(realpath "$(dirname $0)")
 sudo -v -p "Please enter the password for ${USER} host setup can do its thing: "
 
 sudo ${BASE_DIR}/install_docker_dependencies ${USER}
-sudo ~/true_battlebot/docker/scripts/install_nvidia_container_toolkit
+sudo ${BASE_DIR}/install_nvidia_container_toolkit
 
 mkdir -p /media/storage/true-battlebot-media/logs
 sudo chown -R 1000:1000 /media/storage/true-battlebot-media
