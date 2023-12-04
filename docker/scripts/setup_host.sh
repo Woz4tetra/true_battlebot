4c6ecd70a5852036591a489fcfa1344190f1ac41
Woz4tetra <woz4tetra@gmail.com>
2023-12-03 21:23:13 -0500

    fix permissions

--- docker/scripts/setup_host.sh
+++ docker/scripts/setup_host.sh
@@ -6,7 +6,7 @@ sudo -v -p "Please enter the password for ${USER} host setup can do its thing: "
 sudo ${BASE_DIR}/install_docker_dependencies ${USER}
 sudo ${BASE_DIR}/install_nvidia_container_toolkit
 
-mkdir -p /media/storage/true-battlebot-media/logs
+sudo mkdir -p /media/storage/true-battlebot-media/logs
 sudo chown -R 1000:1000 /media/storage/true-battlebot-media
 
 sudo cp ${BASE_DIR}/99-zed.rules /etc/udev/rules.d
