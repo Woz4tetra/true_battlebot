f7c0c914fc8398229214e8fc948a8a7281deabfb
Woz4tetra <woz4tetra@gmail.com>
2023-12-06 11:34:17 -0500

    switch to building detectron

--- docker/resources/install/install_python_torch.sh
+++ docker/resources/install/install_python_torch.sh
@@ -6,6 +6,11 @@ BASE_DIR=$(realpath "$(dirname "${0}")")
 
 sudo chown -R 1000:1000 ${HOME}/.local
 
+git clone https://github.com/facebookresearch/detectron2.git
+cd detectron2
+git checkout 5de5319a49e7ea819586d3d6a817632392dfdeb2
+python -m pip install -e detectron2
+
 sudo -H python -m pip install -r ${BASE_DIR}/torch-requirements.txt
 
 sudo -H python -m pip uninstall -y kiwisolver
