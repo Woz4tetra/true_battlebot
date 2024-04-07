6378dd15cc64fda7af2535f41f780cbf0aa344d4
Woz4tetra <woz4tetra@gmail.com>
2024-04-07 04:01:57 -0400

    implement comparison

--- simulation/TrueBattleBotSim/Assets/Scripts/SerializableTuple.cs
+++ simulation/TrueBattleBotSim/Assets/Scripts/SerializableTuple.cs
@@ -22,21 +22,58 @@ public class SerializableTuple<T1, T2> : IStructuralComparable, IStructuralEquat
 
     public int CompareTo(object other, IComparer comparer)
     {
-        throw new NotImplementedException(); // TODO
+        if (other == null)
+        {
+            return 1;
+        }
+        if (!(other is SerializableTuple<T1, T2>))
+        {
+            throw new ArgumentException("Argument must be a SerializableTuple<T1, T2>", nameof(other));
+        }
+        SerializableTuple<T1, T2> otherTuple = (SerializableTuple<T1, T2>)other;
+        int cmp = comparer.Compare(item1, otherTuple.item1);
+        if (cmp != 0)
+        {
+            return cmp;
+        }
+        return comparer.Compare(item2, otherTuple.item2);
     }
 
     public int CompareTo(object obj)
     {
-        throw new NotImplementedException(); // TODO
+        if (obj == null)
+        {
+            return 1;
+        }
+        if (!(obj is SerializableTuple<T1, T2>))
+        {
+            throw new ArgumentException("Argument must be a SerializableTuple<T1, T2>", nameof(obj));
+        }
+        SerializableTuple<T1, T2> otherTuple = (SerializableTuple<T1, T2>)obj;
+        int cmp = Comparer.Default.Compare(item1, otherTuple.item1);
+        if (cmp != 0)
+        {
+            return cmp;
+        }
+        return Comparer.Default.Compare(item2, otherTuple.item2);
     }
 
     public bool Equals(object other, IEqualityComparer comparer)
     {
-        throw new NotImplementedException(); // TODO
+        if (other == null)
+        {
+            return false;
+        }
+        if (!(other is SerializableTuple<T1, T2>))
+        {
+            return false;
+        }
+        SerializableTuple<T1, T2> otherTuple = (SerializableTuple<T1, T2>)other;
+        return comparer.Equals(item1, otherTuple.item1) && comparer.Equals(item2, otherTuple.item2);
     }
 
     public int GetHashCode(IEqualityComparer comparer)
     {
-        throw new NotImplementedException(); // TODO
+        return HashCode.Combine(comparer.GetHashCode(item1), comparer.GetHashCode(item2));
     }
 }
