59dd9237f83477d029eab04aea679c94d9f94e09
Woz4tetra <woz4tetra@gmail.com>
2024-04-17 11:28:06 -0400

    split dev containers. moving training

--- .devcontainer/setup_container.sh
+++ .devcontainer/setup_container.sh
@@ -4,27 +4,11 @@ export DIR=$(dirname $(realpath ${BASH_SOURCE[0]}))
 
 cd ${DIR}
 
-which nvidia-smi > /dev/null
-if [[ $? -eq 1 ]]; then
-	HAS_GPU="disable"
-else
-	nvidia-smi -L > /dev/null
-	num_gpus=$?
-	if ((num_gpus == 0)); then
-		HAS_GPU="enable"
-	else
-		HAS_GPU="disable"
-	fi
-fi
-
-PROJECT_NAME=$(${DIR}/../docker/get_project_name)
-ln -sf docker-compose.${PROJECT_NAME}.yaml docker-compose.yaml
-
 cat <<EOT > ${DIR}/.env
 ORGANIZATION=$(${DIR}/../docker/get_organization)
 IMAGE_VERSION=$(${DIR}/../docker/get_image_tag)
+PERCEPTION_IMAGE_VERSION=$(${DIR}/../docker/get_image_tag perception)
 PROJECT_DIR=$(realpath "${DIR}/../")
 PROJECT_NAME=$(${DIR}/../docker/get_project_name)
 EOT
 
-${DIR}/../docker/scripts/copy_project
