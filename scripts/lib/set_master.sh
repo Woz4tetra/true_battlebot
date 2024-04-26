e9414c75300de7817e7e0bc56556a2307379bbc3
Woz4tetra <woz4tetra@gmail.com>
2024-04-26 00:16:24 -0400

    better start script

--- scripts/lib/set_master.sh
+++ scripts/lib/set_master.sh
@@ -8,7 +8,7 @@ if [ -n "${INTERFACE}" ]; then
     INTERFACE_IP=$(ifconfig "${INTERFACE}" | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 
     if [[ $INTERFACE_IP =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-        HOST_MACHINE=INTERFACE_IP
+        HOST_MACHINE=${INTERFACE_IP}
     fi
 fi
 
