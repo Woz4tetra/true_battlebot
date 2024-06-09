706783384fe9a3081a0836e43b88ffcb2a5049ae
Woz4tetra <woz4tetra@gmail.com>
2024-06-08 23:20:28 -0400

    has gpu script

--- .devcontainer/setup_container.sh
+++ .devcontainer/setup_container.sh
@@ -6,18 +6,7 @@ cd ${DIR}
 PROJECT_DIR=$(realpath "${DIR}/../")
 DOCKER_ROOT_DIR=${PROJECT_DIR}/docker
 
-which nvidia-smi > /dev/null
-if [[ $? -eq 1 ]]; then
-	HAS_GPU="disable"
-else
-	nvidia-smi -L > /dev/null
-	num_gpus=$?
-	if ((num_gpus == 0)); then
-		HAS_GPU="enable"
-	else
-		HAS_GPU="disable"
-	fi
-fi
+HAS_GPU=$(${DOCKER_ROOT_DIR}/scripts/has_gpu)
 
 ln -sf docker-compose.gpu-${HAS_GPU}.yaml docker-compose.gpu.yaml
 
