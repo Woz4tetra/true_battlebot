2b376811d8ac604c8edf10838600b305c3933943
Woz4tetra <woz4tetra@gmail.com>
2024-07-21 01:10:23 -0400

    keypoint train fixes

--- perception/training/keypoints/train.py
+++ perception/training/keypoints/train.py
@@ -1,14 +1,14 @@
 from ultralytics import YOLO
 
 # Load the model.
-model = YOLO("yolov8m-pose.pt")
+model = YOLO("yolov8x-pose.pt")
 
 # Training.
 results = model.train(
-    data="/media/storage/training/labeled/true-battlebot-keypoints/2024-06-06/data.yaml",
+    data="/media/storage/training/labeled/keypoints/2024-07-20/data.yaml",
     imgsz=1280,
-    epochs=50,
+    epochs=100,
     batch=4,
     name="battlebots_keypoints",
-    save_dir="/media/storage/true-battlebot-media/training/models/battlebots/yolov8-pose",
+    save_dir="/media/storage/true-battlebot-media/training/models/battlebots/yolov8-pose/2024-07-20",
 )
