0eaabcdbd482f12ef3c94a87c1191a8327fe7658
Woz4tetra <woz4tetra@gmail.com>
2024-07-30 20:24:09 -0400

    use variables

--- simulation/TrueBattleBotSim/copy_persistent_data.py
+++ simulation/TrueBattleBotSim/copy_persistent_data.py
@@ -4,6 +4,8 @@ import shutil
 from pathlib import Path
 
 SCRIPT_DIR = Path(os.path.dirname(os.path.realpath(__file__)))
+PROJECT_NAME = "TrueBattleBotSim"
+COMPANY_NAME = "DefaultCompany"
 
 
 def main() -> None:
@@ -12,13 +14,11 @@ def main() -> None:
 
     source_path = SCRIPT_DIR / "Assets" / "PersistentData"
     if current_platform == "Windows":
-        destination_path = (
-            Path(os.environ["USERPROFILE"]) / "Appdata" / "LocalLow" / "DefaultCompany" / "TrueBattleBotSim"
-        )
+        destination_path = Path(os.environ["USERPROFILE"]) / "Appdata" / "LocalLow" / COMPANY_NAME / PROJECT_NAME
     elif current_platform == "Linux":
-        destination_path = Path.home() / ".config" / "unity3d" / "DefaultCompany" / "TrueBattleBotSim"
+        destination_path = Path.home() / ".config" / "unity3d" / COMPANY_NAME / PROJECT_NAME
     elif current_platform == "Darwin":
-        destination_path = Path.home() / "Library" / "Application Support" / "unity.DefaultCompany.TrueBattleBotSim"
+        destination_path = Path.home() / "Library" / "Application Support" / f"unity.{COMPANY_NAME}.{PROJECT_NAME}"
     else:
         raise NotImplementedError(f"Platform {current_platform} is not supported!")
 
