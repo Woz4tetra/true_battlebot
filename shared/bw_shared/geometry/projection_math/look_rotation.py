6b03369d3dd634ace8bc40b697efd17fbcaab9d8
Woz4tetra <woz4tetra@gmail.com>
2024-08-22 01:47:07 -0400

    formatting

--- shared/bw_shared/geometry/projection_math/look_rotation.py
+++ shared/bw_shared/geometry/projection_math/look_rotation.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
 import numpy as np
-from geometry_msgs.msg import Quaternion, Vector3
+from geometry_msgs.msg import Quaternion
 
 from bw_shared.geometry.rotation_transforms import quaternion_from_matrix
 
