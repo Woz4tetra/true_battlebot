426ea6473b825ddcdb2fa518c04ebd81940a3e9a
Woz4tetra <woz4tetra@gmail.com>
2024-09-20 20:59:22 -0400

    auto restart

--- docker/resources/launch/launch.sh
+++ docker/resources/launch/launch.sh
@@ -9,13 +9,14 @@ if [ $? != 0 ]; then
     tmux new -s "${SESSION}" -d -x 200 -y 24
 fi
 
-if [ ! -f /opt/"${ORGANIZATION}"/roslaunch_pid ]; then
-    echo 0 > /opt/"${ORGANIZATION}"/roslaunch_pid
-fi
+APP_PID=/opt/"${ORGANIZATION}"/app_pid
+
+echo 0 > ${APP_PID}
+
 # SIGINT-handler
 int_handler() {
     echo "Caught stop signal"
-    pid=$(cat /opt/"${ORGANIZATION}"/roslaunch_pid)
+    pid=$(cat ${APP_PID})
     if [ "${pid}" -ne 0 ]; then
         echo "Stopping roslaunch"
         kill -SIGINT "${pid}"
@@ -25,10 +26,22 @@ int_handler() {
 }
 trap 'int_handler' SIGINT
 
+wait_for_pid() {
+    # Loop until the pid isn't zero
+    while [ "$(cat "${APP_PID}")" -eq 0 ]; do
+        sleep 1
+    done
+}
+
 tmux send -t "${SESSION}" 'source /opt/${ORGANIZATION}/scripts/lib/enable_tmux_logger.sh ${PROJECT_NAME} ${PROJECT_NAME}' ENTER
 tmux send -t "${SESSION}" 'source /opt/${ORGANIZATION}/scripts/lib/set_master.sh ${ROS_MASTER_INTERFACE}' ENTER
 tmux send -t "${SESSION}" 'source /opt/${ORGANIZATION}/set_log_format.sh' ENTER
 tmux send -t "${SESSION}" 'roslaunch --wait bw_bringup bw_bringup.launch --screen &' ENTER
-tmux send -t "${SESSION}" 'echo $! > /opt/${ORGANIZATION}/roslaunch_pid' ENTER
+tmux send -t "${SESSION}" 'echo $! > /opt/${ORGANIZATION}/app_pid' ENTER
 
-sleep infinity
+wait_for_pid
+pid=$(cat ${APP_PID})
+tail --pid=$pid -f /dev/null
+tmux kill-session -t "${SESSION}"
+
+echo "${PROJECT_NAME} exited"
--- docker/resources/launch/perception.sh
+++ docker/resources/launch/perception.sh
@@ -9,13 +9,14 @@ if [ $? != 0 ]; then
     tmux new -s "${SESSION}" -d -x 200 -y 24
 fi
 
-if [ ! -f /opt/"${ORGANIZATION}"/app_pid ]; then
-    echo 0 > /opt/"${ORGANIZATION}"/app_pid
-fi
+APP_PID=/opt/"${ORGANIZATION}"/app_pid
+
+echo 0 > ${APP_PID}
+
 # SIGINT-handler
 int_handler() {
     echo "Caught stop signal"
-    pid=$(cat /opt/"${ORGANIZATION}"/app_pid)
+    pid=$(cat ${APP_PID})
     if [ "${pid}" -ne 0 ]; then
         echo "Stopping perception"
         kill -SIGINT "${pid}"
@@ -25,10 +26,22 @@ int_handler() {
 }
 trap 'int_handler' SIGINT
 
+wait_for_pid() {
+    # Loop until the pid isn't zero
+    while [ "$(cat "${APP_PID}")" -eq 0 ]; do
+        sleep 1
+    done
+}
+
 tmux send -t "${SESSION}" 'source /opt/${ORGANIZATION}/scripts/lib/enable_tmux_logger.sh ${PROJECT_NAME}_perception ${PROJECT_NAME}_perception' ENTER
 tmux send -t "${SESSION}" 'source /opt/${ORGANIZATION}/scripts/lib/set_master.sh ${ROS_MASTER_INTERFACE}' ENTER
 tmux send -t "${SESSION}" 'cd /opt/${ORGANIZATION}/${PROJECT_NAME}' ENTER
 tmux send -t "${SESSION}" 'python perception/detection/main.py perception/configs &' ENTER
 tmux send -t "${SESSION}" 'echo $! > /opt/${ORGANIZATION}/app_pid' ENTER
 
-sleep infinity
+wait_for_pid
+pid=$(cat ${APP_PID})
+tail --pid=$pid -f /dev/null
+tmux kill-session -t "${SESSION}"
+
+echo "${PROJECT_NAME} perception exited"
