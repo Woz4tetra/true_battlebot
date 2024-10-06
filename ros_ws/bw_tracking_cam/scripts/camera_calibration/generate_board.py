f2539b2d330da7ddf88364984a0c3986e6533e86
Woz4tetra <woz4tetra@gmail.com>
2024-10-05 22:14:48 -0400

    generate pdfs

--- ros_ws/bw_tracking_cam/scripts/camera_calibration/generate_board.py
+++ ros_ws/bw_tracking_cam/scripts/camera_calibration/generate_board.py
@@ -3,6 +3,7 @@ import os
 
 import cv2
 from bw_shared.camera_calibration.board_config import BoardConfig
+from PIL import Image
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -11,8 +12,17 @@ if __name__ == "__main__":
 
     board_config_path = args.board_config
     image_path = os.path.splitext(board_config_path)[0] + ".png"
+    pdf_path = os.path.splitext(board_config_path)[0] + ".pdf"
 
     config = BoardConfig.from_file(board_config_path)
 
-    cv2.imwrite(image_path, config.generate_image())
-    print(f"Generated board image at {image_path}")
+    board_image = config.generate_image()
+    pil_image = Image.fromarray(board_image)
+
+    cv2.imwrite(image_path, board_image)
+    print(f"Generated board image ({board_image.shape[1]}x{board_image.shape[0]}) at {image_path}")
+
+    dpi = config.px_per_meter * 0.0254
+
+    pil_image.save(pdf_path, "PDF", resolution=300, save_all=True)
+    print(f"Generated board pdf at {pdf_path}. (DPI: {dpi})")
