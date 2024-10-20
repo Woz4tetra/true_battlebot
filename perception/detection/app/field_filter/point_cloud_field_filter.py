opponent mini bot. fix field rotation bugs

perception/detection/app/field_filter/point_cloud_field_filter.py
@@ -6,6 +6,7 @@ from app.field_filter.field_filter_interface import FieldFilterInterface
 from app.field_filter.helpers import get_field
 from app.field_filter.solvers.base_plane_solver import BasePlaneSolver
 from bw_interfaces.msg import EstimatedObject, SegmentationInstanceArray
+from bw_shared.geometry.input_modulus import normalize_angle
 from bw_shared.geometry.projection_math.find_minimum_rectangle import (
     find_minimum_rectangle,
     get_rectangle_angle,
@@ -35,8 +36,8 @@ class PointCloudFieldFilter(FieldFilterInterface):
         inlier_points = filtered_points[plane_inliers]
         plane_center = np.mean(inlier_points, axis=0)
         self.logger.debug(
-            f"Plane normal: {plane_normal}. "
-            f"Plane center: {plane_center}. "
+            f"Plane normal: {plane_normal.tolist()}. "
+            f"Plane center: {plane_center.tolist()}. "
             f"Number of inliers: {np.sum(plane_inliers)}."
         )
         self.logger.debug(f"Inlier points shape: {inlier_points.shape}")
@@ -55,13 +56,21 @@ class PointCloudFieldFilter(FieldFilterInterface):
         extents = get_rectangle_extents(min_rect)  # get the 2D bounds of the rectangle
         # get the angle of the rectangle with respect to and nearest to the x-axis
         angle = get_rectangle_angle(min_rect)
+        complimentary_angle = normalize_angle(angle + np.pi)
+        if abs(complimentary_angle) < abs(angle):
+            self.logger.debug(f"Selecting complimentary angle: {complimentary_angle} over {angle}.")
+            angle = complimentary_angle
+        if angle > 0:
+            extents = XY(extents.y, extents.x)
         centroid = np.mean(min_rect, axis=0)  # compute 2D centroid
         # transform the flattened 2D pose back to 3D
         flat_transform = Transform3D.from_position_and_rpy(Vector3(centroid[0], centroid[1], 0.0), RPY((0, 0, angle)))
         field_centered_plane = flat_transform.forward_by(plane_transform)
         self.logger.debug(f"Flat transform: {field_centered_plane}")
         self.logger.debug(f"Field centered plane transform: {field_centered_plane}")
-        self.logger.debug(f"Minimum rectangle: {min_rect}. Rectangle extents: {extents}. Rectangle angle: {angle}.")
+        self.logger.debug(
+            f"Minimum rectangle: {min_rect.tolist()}. Rectangle extents: {extents}. Rectangle angle: {angle}."
+        )
         return field_centered_plane, extents, inlier_points
 
     def compute_field(

shared/bw_shared/geometry/projection_math/find_minimum_rectangle.py
@@ -1,7 +1,6 @@
 import numpy as np
 from scipy.spatial import ConvexHull
 
-from bw_shared.geometry.input_modulus import normalize_angle
 from bw_shared.geometry.xy import XY
 
 
@@ -75,19 +74,14 @@ def get_rectangle_angle(rectangle: np.ndarray) -> float:
     rectangle is a 4x2 matrix of points
     """
     angles = []
+    midpoints = []
     for index in range(len(rectangle) - 1):
         p0 = rectangle[index]
         p1 = rectangle[index + 1]
         delta = p1 - p0
         angle = np.arctan2(delta[1], delta[0])
         angles.append(angle)
+        midpoints.append((p0 + p1) / 2)
     angles_array = np.array(angles)
-    compliment_angles = normalize_angle(angles_array + np.pi)
-    smallest_angle = np.argmin(np.abs(angles_array))
-    smallest_compliment_angle = np.argmin(np.abs(compliment_angles))
-    smallest_index = (
-        smallest_angle
-        if angles_array[smallest_angle] < compliment_angles[smallest_compliment_angle]
-        else smallest_compliment_angle
-    )
-    return normalize_angle(angles_array[smallest_index] + np.pi)
+    midpoints_array = np.array(midpoints)
+    return angles_array[np.argmin(midpoints_array[:, 1])]