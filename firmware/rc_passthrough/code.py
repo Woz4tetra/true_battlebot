104fc7ffe7bd662b12bc638d84f9b1cebc9ab3d8
Woz4tetra <woz4tetra@gmail.com>
2025-02-16 00:10:27 -0500

    passthrough working with transmitter

--- firmware/rc_passthrough/code.py
+++ firmware/rc_passthrough/code.py
@@ -1,11 +1,9 @@
 import os
 import time
-import pwmio
+
 import board
 import digitalio
-
-import time
-import board
+import pulseio
 import pwmio
 from adafruit_motor import servo
 
@@ -34,7 +32,45 @@ def arange(*args):
         while stop < value:
             yield value
             value += step
-        
+
+
+def get_sticks():
+    channel1 = pulseio.PulseIn(board.GP14, maxlen=2, idle_state=False)
+    channel1.resume(1)
+    time.sleep(0.04)
+    freq1, cycle1 = read_pwm(channel1)
+    channel1.deinit()
+    channel2 = pulseio.PulseIn(board.GP15, maxlen=2, idle_state=False)
+    channel2.resume(1)
+    time.sleep(0.04)
+    freq2, cycle2 = read_pwm(channel2)
+    channel2.deinit()
+
+    if freq1 is None:
+        cycle1 = None
+    if freq2 is None:
+        cycle2 = None
+    if cycle1 == 0:
+        cycle1 = None
+    if cycle2 == 0:
+        cycle2 = None
+    return cycle1, cycle2
+
+
+def read_pwm(pwm_in):
+    if len(pwm_in) < 2:
+        return None, 0
+    pwm_in.pause()
+    high_time = pwm_in[0]
+    low_time = pwm_in[1]
+    period = high_time + low_time
+    # print(high_time,low_time, period)
+    frequency = 1 / (period * 1e-6)  # Convert microseconds to seconds and then to frequency
+    duty_cycle = high_time / (period) * 100  # Calculate duty cycle percentage
+    if duty_cycle < 50:
+        return None, duty_cycle
+    pwm_in.clear()
+    return frequency, duty_cycle
 
 
 # import wifi
@@ -51,11 +87,9 @@ for _ in range(10):
     led.value = False
     time.sleep(0.01)
 
-# create a PWMOut object on Pin A2.
-pwm0 = pwmio.PWMOut(board.GP2, duty_cycle=2 ** 15, frequency=400)
-pwm1 = pwmio.PWMOut(board.GP3, duty_cycle=2 ** 15, frequency=400)
+pwm0 = pwmio.PWMOut(board.GP2, duty_cycle=2**15, frequency=400)
+pwm1 = pwmio.PWMOut(board.GP3, duty_cycle=2**15, frequency=400)
 
-# Create a servo object, left_servo.
 left_servo = servo.Servo(pwm0)
 right_servo = servo.Servo(pwm1)
 
@@ -77,24 +111,65 @@ right_servo.angle = 90
 
 time.sleep(1)
 
-increment = 0.5
-delay = 0.0025
+increment = 1.5
+delay = 0.025
 neutral_val = 90
 spread = 60
 max_val = neutral_val + spread
 min_val = neutral_val - spread
+max_cycle = 95.0652
+min_cycle = 89.945
 
+
+def scale_percent_to_angle(signed_percent):
+    angle = (max_val - min_val) / 200 * (signed_percent + 100) + min_val
+    return int(min(180, max(0, angle)))
+
+
+def scale_cycle_to_percent(duty_cycle):
+    percent = -200.0 / (max_cycle - min_cycle) * (duty_cycle - min_cycle) + 100.0
+    return min(100.0, max(-100.0, percent))
+
+
+up_down = 0
+left_right = 0
+
+max_up_down = None
+max_left_right = None
+min_up_down = None
+min_left_right = None
 while True:
-    for angle in arange(neutral_val, max_val, increment):
-        left_servo.angle = angle
-        right_servo.angle = angle
-        time.sleep(delay)
-    for angle in arange(max_val, min_val, -increment):
-        left_servo.angle = angle
-        right_servo.angle = angle
-        time.sleep(delay)
-    for angle in arange(min_val, neutral_val, increment):
-        left_servo.angle = angle
-        right_servo.angle = angle
-        time.sleep(delay)
+    next_left_right, next_up_down = get_sticks()
+    if next_up_down is not None:
+        up_down = next_up_down
+        max_up_down = max(max_up_down, up_down) if max_up_down is not None else up_down
+        max_left_right = max(max_left_right, left_right) if max_left_right is not None else left_right
+    if next_left_right is not None:
+        left_right = next_left_right
+        min_up_down = min(min_up_down, up_down) if min_up_down is not None else up_down
+        min_left_right = min(min_left_right, left_right) if min_left_right is not None else left_right
 
+    # print("\t".join([str(x) for x in (max_up_down, max_left_right, min_up_down, min_left_right)]))
+    up_down_percent = -1 * scale_cycle_to_percent(up_down)
+    left_right_percent = scale_cycle_to_percent(left_right)
+
+    left = up_down_percent - left_right_percent
+    right = up_down_percent + left_right_percent
+    left_servo.angle = scale_percent_to_angle(left)
+    right_servo.angle = scale_percent_to_angle(right)
+
+    # for angle in arange(neutral_val, max_val, increment):
+    #     left_servo.angle = angle
+    #     right_servo.angle = angle
+    #     time.sleep(delay)
+    #     print_analog()
+    # for angle in arange(max_val, min_val, -increment):
+    #     left_servo.angle = angle
+    #     right_servo.angle = angle
+    #     time.sleep(delay)
+    #     print_analog()
+    # for angle in arange(min_val, neutral_val, increment):
+    #     left_servo.angle = angle
+    #     right_servo.angle = angle
+    #     time.sleep(delay)
+    #     print_analog()
