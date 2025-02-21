4cd9df6ac80469d3fee180e62d5343ce444a6167
Woz4tetra <woz4tetra@gmail.com>
2025-02-21 03:29:19 -0500

    convert to leonardo

--- firmware/rc_bridge/src/main.cpp
+++ firmware/rc_bridge/src/main.cpp
@@ -1,31 +1,29 @@
 #include <Arduino.h>
 #include <Servo.h>
-#include <Adafruit_DotStar.h>
-#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_ADXL375.h>
 
-#define NUMPIXELS 1
-#define DATAPIN 7
-#define CLOCKPIN 8
-Adafruit_DotStar strip = Adafruit_DotStar(NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BGR);
-unsigned int rainbow_color_index = 0;
+float led_intensity = 0.0;
+int led_cycle_index = 0;
+int led_cycle_direction = 1;
 
-#define CHANNEL_A 2
+#define CHANNEL_A 1
 #define CHANNEL_B 4
 
-const unsigned long PWM_TIMEOUT = 50000;
 const unsigned long LOW_PERIOD = 19000;  // 19 millisecond (nominally 50 Hz)
 const unsigned long HIGH_PERIOD = 21000; // 21 millisecond (nominally 50 Hz)
 volatile unsigned long pulse_in_begin_a = 0, pulse_in_begin_b = 0;
 volatile unsigned long pulse_in_end_a = 0, pulse_in_end_b = 0;
 volatile unsigned long pulse_duration_a = 0, pulse_duration_b = 0;
 volatile bool pulse_in_available_a = false, pulse_in_available_b = false;
-float duty_cycle_a, duty_cycle_b;
+float duty_cycle_a = 0.0, duty_cycle_b = 0.0;
 
 const float min_cycle = 5.0;
 const float max_cycle = 10.0;
 
-#define SERVO_LEFT 1
-#define SERVO_RIGHT 3
+#define SERVO_LEFT 3
+#define SERVO_RIGHT 13
 
 Servo servo_left;
 Servo servo_right;
@@ -34,6 +32,13 @@ const int neutral_pulse = 1500;
 const int spread_pulse = 500;
 const int max_pulse = neutral_pulse + spread_pulse;
 const int min_pulse = neutral_pulse - spread_pulse;
+
+float right_side_up_threshold = 70.0;
+float upside_down_threshold = 140.0;
+bool is_upside_down = false;
+
+Adafruit_ADXL375 accel = Adafruit_ADXL375(12345);
+bool accel_initialized = false;
 
 float scale_cycle_to_percent(float duty_cycle)
 {
@@ -120,46 +125,84 @@ void write_escs(int left_pulse, int right_pulse)
 void initialize_escs()
 {
     write_escs(MIN_PULSE_WIDTH, MIN_PULSE_WIDTH);
-    delay(3000);
+    delay(2000);
     write_escs(neutral_pulse, neutral_pulse);
-    delay(1000);
+    delay(500);
 }
 
-void cycle_rainbow_led(float intensity)
+void set_builtin_led(int value)
 {
-    strip.setPixelColor(0, strip.ColorHSV(rainbow_color_index, 255, 255));
-    strip.setBrightness((int)(intensity * 0.01 * 255));
-    strip.show();
-    rainbow_color_index = (rainbow_color_index + 1000) % 65535;
+    analogWrite(LED_BUILTIN, value);
+}
+
+void cycle_led(float intensity)
+{
+    set_builtin_led(led_cycle_index);
+    led_cycle_index += led_cycle_direction;
+    if (led_cycle_index < 0 || led_cycle_index >= 255)
+    {
+        led_cycle_direction *= -1;
+    }
+    Serial.println(led_cycle_index);
+}
+
+void pulse_led()
+{
+    for (int count = 0; count < 255; count += 5)
+    {
+        set_builtin_led(count);
+        delay(1);
+    }
+    for (int count = 255; count > 0; count -= 5)
+    {
+        set_builtin_led(count);
+        delay(1);
+    }
+    set_builtin_led(0);
 }
 
 void setup()
 {
-    strip.begin();
-    strip.show();
-
-    strip.setPixelColor(0, 0xFF0000); // red
-    strip.show();
-
-    Serial.begin(115200);
-    pinMode(CHANNEL_A, INPUT);
-    pinMode(CHANNEL_B, INPUT);
-
-    attachInterrupt(digitalPinToInterrupt(CHANNEL_A), channel_a_interrupt, CHANGE);
-    attachInterrupt(digitalPinToInterrupt(CHANNEL_B), channel_b_interrupt, CHANGE);
+    pinMode(LED_BUILTIN, OUTPUT);
+    set_builtin_led(0);
 
     servo_left.attach(SERVO_LEFT);
     servo_right.attach(SERVO_RIGHT);
-
-    Serial.println("Initializing ESCs");
-
-    strip.setPixelColor(0, 0x00FF00); // green
-    strip.show();
-
     initialize_escs();
 
-    strip.setPixelColor(0, 0x0000FF); // blue
-    strip.show();
+    pinMode(CHANNEL_A, INPUT);
+    pinMode(CHANNEL_B, INPUT);
+    attachInterrupt(digitalPinToInterrupt(CHANNEL_A), channel_a_interrupt, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(CHANNEL_B), channel_b_interrupt, CHANGE);
+
+    accel_initialized = accel.begin();
+    if (!accel_initialized)
+    {
+        for (int count = 0; count < 10; count++)
+        {
+            pulse_led();
+        }
+    }
+
+    set_builtin_led(255);
+}
+
+float angle_from_vertical()
+{
+    if (!accel_initialized)
+        return 0.0;
+    sensors_event_t event;
+    accel.getEvent(&event);
+
+    float x = event.acceleration.x;
+    float y = event.acceleration.y;
+    float z = event.acceleration.z;
+
+    float magnitude = sqrt(x * x + y * y + z * z);
+    z = z / magnitude;
+
+    float clipped_z = min(1.0, max(-1.0, z));
+    return acos(clipped_z) * 180.0 / PI;
 }
 
 void loop()
@@ -175,24 +218,36 @@ void loop()
         read_pwm(high_duration_b, low_duration_b, next_duty_cycle_b))
         duty_cycle_b = next_duty_cycle_b;
 
-    float a_percent = scale_cycle_to_percent(duty_cycle_a);
-    float b_percent = -1 * scale_cycle_to_percent(duty_cycle_b);
+    float a_percent, b_percent;
+    if (duty_cycle_a == 0.0)
+        a_percent = 0.0;
+    else
+        a_percent = scale_cycle_to_percent(duty_cycle_a);
 
-    float led_intensity = (abs(a_percent) + abs(b_percent)) / 2.0;
+    if (duty_cycle_b == 0.0)
+        b_percent = 0.0;
+    else
+        b_percent = -1 * scale_cycle_to_percent(duty_cycle_b);
+
+    led_intensity = (abs(a_percent) + abs(b_percent)) / 2.0;
+
+    float angle = angle_from_vertical();
+    if (angle < right_side_up_threshold)
+        is_upside_down = false;
+    else if (angle > upside_down_threshold)
+        is_upside_down = true;
+
+    if (is_upside_down)
+        a_percent *= -1;
 
     float left_command = a_percent + b_percent;
     float right_command = a_percent - b_percent;
-
-    Serial.print("L: ");
-    Serial.print(left_command);
-    Serial.print(" R: ");
-    Serial.println(right_command);
 
     int left_pulse = scale_percent_to_pulse(left_command);
     int right_pulse = scale_percent_to_pulse(right_command);
 
     write_escs(left_pulse, right_pulse);
 
-    cycle_rainbow_led(led_intensity);
     delay(10);
+    cycle_led(led_intensity);
 }
